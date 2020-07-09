7b60a0fc071856dc7aa5db64cec7eb0a5f9a9acc
Saurabh7120 <saurabh.mane@ob10it.com>
2020-07-10 00:38:48 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+/*jshint esversion :9*/
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar-component';
+import {MuiThemeProvider,createMuiTheme,CssBaseline} from '@material-ui/core';
 
+const theme = createMuiTheme();
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme = {theme}>
+    <CssBaseline/>
+    <div>
+      <Navbar/>
       <h1>Melofobia</h1>
     </div>
+    </MuiThemeProvider>
   );
 }
 
