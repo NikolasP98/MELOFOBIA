80891da2746aa7113a2e70c89586422064ca115b
SaurabhOb10 <saurabh.mane@ob10it.com>
2020-07-10 20:21:05 +0530

    Added navbar and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar-component';
+import Footer from './Components/Footer/footer-component';
 import {MuiThemeProvider,createMuiTheme,CssBaseline} from '@material-ui/core';
 
 const theme = createMuiTheme();
@@ -12,6 +13,7 @@ function App() {
     <div>
       <Navbar/>
       <h1>Melofobia</h1>
+      <Footer/>
     </div>
     </MuiThemeProvider>
   );
