06fadcb8d78b9d9ce389f72b7b91e9ad8bfc9a95
Your Name <ushinigami97@gmail.com>
2020-07-16 17:04:14 +0200

    Update article-component.jsx

--- src/Components/Article/article-component.jsx
+++ src/Components/Article/article-component.jsx
@@ -4,7 +4,49 @@ import PropTypes from 'prop-types'
 function Article() {
     return (
         <div>
-            
+            <div>ARTICLE</div>
+            <div>
+                <h1>ARTICLE TITLE</h1>
+                <p>autor: Nombre Autor</p>
+                <p>published time and date</p>
+            </div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit.
+                        Suspendisse nibh nulla, dapibus
+                        sit amet felis et, posuere
+                        tincidunt nisl. In hac habitasse
+                        platea dictumst. Pellentesque
+                        habitant morbi tristique senectus
+                        et netus et malesuada fames ac
+                        turpis egestas. Nullam eget
+                        ultrices purus, id porttitor
+                        odio. Proin sed ligula sed tortor
+                        ornare auctor. Fusce ut ipsum
+                        quis sapien faucibus sodales eget
+                        et dolor. Nam facilisis risus et
+                        sem vestibulum hendrerit. Nullam
+                        facilisis at dolor in faucibus.
+                        Sed condimentum gravida ligula,
+                        id accumsan nulla iaculis vitae.
+                        Ut non dictum erat. Praesent
+                        dignissim varius turpis id
+                        venenatis. Aenean vestibulum
+                        auctor purus auctor tempor.
+                        Praesent cursus nunc non diam
+                        varius, vel suscipit dui ornare.
+                        Donec commodo nisl nec eleifend
+                        bibendum. Vivamus a tellus ac est
+                        egestas facilisis nec eget nunc.
+                        Morbi eget imperdiet metus.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
