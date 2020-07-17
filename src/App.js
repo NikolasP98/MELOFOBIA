3c1fce5f1f8e78fbd2c86f97a9f52b982155d43d
Your Name <ushinigami97@gmail.com>
2020-07-17 02:52:41 +0200

    Add react router dom
    editor is on localhost:3000/editor

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar-component';
 import Footer from './Components/Footer/footer-component';
-import {MuiThemeProvider,createMuiTheme,CssBaseline} from '@material-ui/core';
+import { MuiThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core';
 import Editor from './Components/Editor/editor-component';
 import Article from './Components/Article/article-component';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const theme = createMuiTheme();
 function App() {
   return (
-    <MuiThemeProvider theme = {theme}>
-    <CssBaseline/>
-    <div>
-      <Navbar/>
-      <Editor/>
-      <Footer/>
-    </div>
-    </MuiThemeProvider>
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path='/editor' exact component={ Editor } />
+            <Route path='/article' exact component={ Article } />
+          </Switch>
+          <Footer />
+        </div>
+      </MuiThemeProvider>
+    </Router>
   );
 }
 
