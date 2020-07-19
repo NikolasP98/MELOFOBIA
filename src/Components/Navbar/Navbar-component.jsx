284e23bd6ebb22e78d66ab57dee6bb7655b35928
Your Name <ushinigami97@gmail.com>
2020-07-20 01:44:29 +0200

    Fixed nav brand fixed image, padding etc

--- src/Components/Navbar/Navbar-component.jsx
+++ src/Components/Navbar/Navbar-component.jsx
@@ -1,65 +1,65 @@
 /*jshint esversion:9*/
 
-import React,{useState} from 'react';
-import{SwipeableDrawer,List,ListItem,ListItemText,Divider,AppBar,Toolbar,Button,IconButton,Typography} from '@material-ui/core';
+import React, { useState } from 'react';
+import { SwipeableDrawer, List, ListItem, ListItemText, Divider, AppBar, Toolbar, Button, IconButton, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import './Navbar-style.scss';
 import logo from '../../Assets/melof.png'
 import CloseIcon from '@material-ui/icons/Close';
 
-function Navbar(){
+function Navbar() {
 
-  const [open,setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  return(
+  return (
     <div>
-    <div className='root'>
-  <AppBar position="static">
-    <Toolbar className='root'>
-      <img src={logo} className='title'/>
-      <div className='flex-container'>
-      <IconButton className='menu-logo' onClick={()=>setOpen(true)}  color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
+      <div className='root'>
+        <AppBar position="static">
+          <Toolbar className='root'>
+            <img src={logo} className='title' />
+            <div className='flex-container'>
+              <IconButton className='menu-logo' onClick={() => setOpen(true)} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
-    </Toolbar>
-  </AppBar>
-</div>
-    <React.Fragment>
-          <SwipeableDrawer
+      <React.Fragment>
+        <SwipeableDrawer
 
-            anchor='right'
-            open={open}
-            onOpen={()=>setOpen(true)}
-          >
+          anchor='right'
+          open={open}
+          onOpen={() => setOpen(true)}
+        >
           <div
-    className='list'
-    role="presentation"
-  >
+            className='list'
+            role="presentation"
+          >
 
 
-    <List className='list-container'>
-    <ListItem button className='list-item'>
-    <IconButton onClick={()=>setOpen(false)}>
-      <CloseIcon/>
-    </IconButton>
-    </ListItem>
-        <ListItem button className='list-item'>
-          <ListItemText primary='First Item' className='list-item-text'/>
-        </ListItem>
-        <ListItem button className='list-item'>
-          <ListItemText primary='Second Item' className='list-item-text'/>
-        </ListItem>
-        <ListItem button className='list-item'>
-          <ListItemText primary='Third Item' className='list-item-text'/>
-        </ListItem>
-    </List>
-    <Divider />
-  </div>
-          </SwipeableDrawer>
-        </React.Fragment>
+            <List className='list-container'>
+              <ListItem button className='list-item'>
+                <IconButton onClick={() => setOpen(false)}>
+                  <CloseIcon />
+                </IconButton>
+              </ListItem>
+              <ListItem button className='list-item'>
+                <ListItemText primary='First Item' className='list-item-text' />
+              </ListItem>
+              <ListItem button className='list-item'>
+                <ListItemText primary='Second Item' className='list-item-text' />
+              </ListItem>
+              <ListItem button className='list-item'>
+                <ListItemText primary='Third Item' className='list-item-text' />
+              </ListItem>
+            </List>
+            <Divider />
+          </div>
+        </SwipeableDrawer>
+      </React.Fragment>
 
-  </div>
+    </div>
 
   );
 }
